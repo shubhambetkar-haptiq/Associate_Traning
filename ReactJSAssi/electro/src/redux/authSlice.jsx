526b2500f41a86c9f6ff36294ec296ab0af0c524
shubhambetkar-haptiq <shubham.betkar@haptiq.com>
2025-08-08 15:25:53 +0530

    FR7

--- ReactJSAssi/electro/src/redux/authSlice.jsx
+++ ReactJSAssi/electro/src/redux/authSlice.jsx
@@ -1,7 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// Load from localStorage
-// const tokenFromStorage = localStorage.getItem('token');
+
 const userFromStorage = localStorage.getItem('user');
 
 const initialState = {
@@ -16,19 +15,13 @@ const authSlice = createSlice({
     login: (state, action) => {
       state.isAuthenticated = true;
       state.user = action.payload;
-      // state.token = action.payload.token;
-
-    
-      localStorage.setItem('user', JSON.stringify(action.payload));
+      
+localStorage.setItem('user', JSON.stringify(action.payload));
     },
     logout: (state) => {
       state.isAuthenticated = false;
       state.user = null;
-      // state.token = null;
-
-      // Clear localStorage
-      // localStorage.removeItem('token');
-      localStorage.removeItem('user');
+           localStorage.removeItem('user');
     },
   },
 });
